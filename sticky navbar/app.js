a622445277260abb414d53a3b2c16e360b889e41
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-20 12:54:42 +0200

    jhi

--- sticky navbar/app.js
+++ sticky navbar/app.js
@@ -1,1 +1,12 @@
-const navberEl = document.querySelectorAll(".navbar")
+const navbarEl = document.querySelectorAll(".navbar")
+const bottomContainerEl = document.querySelector(".bottom-container")
+
+console.log(navbarEl.offsetTop)
+console.log(bottomContainerEl.offsetTop)
+window.addEventListener("scroll", () => {
+    if (window.scrollY > (bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50)) {
+        navbarEl.classList.add("active")
+    } else {
+        navbarEl.classList.remove("active")
+    }
+})
