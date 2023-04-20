9f8e4a62f8351e930271cad177f76c7c04ebffd0
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-20 07:39:46 +0200

    Update app.js

--- digital_clock/app.js
+++ digital_clock/app.js
@@ -3,10 +3,25 @@ const minuteEl = document.getElementById("minutes")
 const secondEl = document.getElementById("seconds")
 const ampmhourEl = document.getElementById("ampm")
 
-function updateClock(){
-    let h =new Date().getHours()
-    let m =new Date().getMinutes()
-    let s =new Date().getSeconds()
-    let ampm ="AM"
+function updateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
 
+    if (h > 12) {
+        h -= 12
+        ampm = "PM"
+    }
+    h=h<10?"0"+h:h
+    m=m<10?"0"+m:m
+    s=s<10?"0"+s:s
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmhourEl, (innerText = ampm)
+    setTimeout(() => {
+        updateClock()
+    }, 1000);
 }
+updateClock()
